3388d274ade762eaa10a39662a21e08f7ab67f12
RonnyJB <ronny1590@hotmail.com>
2019-05-28 22:50:54 -0600

    Creación de las clases con gets & sets

---
+++ clases.py
@@ -0,0 +1,88 @@
+class Persona:
+
+    def __init__(self):
+        self.cedula = 0
+        self.nombreCompleto = ""
+        self.telefono = 0
+        self.voto = 0
+
+    def setCedula(self, cedula):
+        self.cedula = cedula
+
+    def getCedula(self):
+        return self.cedula
+
+    def setNombreCompleto(self, nombreCompleto):
+        self.nombreCompleto = nombreCompleto
+
+    def getNombreCompleto(self):
+        return self.nombreCompleto
+
+    def setTelefono(self, telefono):
+        self.telefono = telefono
+
+    def getTelefono(self):
+        return self.telefono
+
+    def setVoto(self, voto):
+        self.voto = voto
+
+    def getVoto(self):
+        return self.voto
+
+class Estudiante(Persona):
+
+    def __init__(self):
+        self.carnet = ""
+        self.carrera = ""
+        Persona.__init__(self)
+
+    def setCarnet(self, carnet):
+        self.carnet = carnet
+
+    def getCarnet(self):
+        return self.carnet
+
+    def setCarrera(self, carrera):
+        self.carrera = carrera
+
+    def getCarrera(self):
+        return self.carrera
+
+class Profesor(Persona):
+
+    def __init__(self):
+        self.publicaciones = ""
+        self.candidato = ""
+        Persona.__init__(self)
+
+    def setPublicaciones(self, publicaciones):
+        self.publicaciones = publicaciones
+
+    def getPublicaciones(self):
+        return self.publicaciones
+
+    def setCandidato(self, candidato):
+        self.candidato = candidato
+
+    def getCandidato(self):
+        return self.candidato
+
+class Administrativo(Persona):
+
+    def __init__(self):
+        self.puesto = ""
+        self.extension = ""
+        Persona.__init__(self)
+
+    def setPuesto(self, puesto):
+        self.puesto = puesto
+
+    def getPuesto(self):
+        return self.puesto
+
+    def setExtension(self, extension):
+        self.extension = extension
+
+    def getExtension(self):
+        return self.extension
